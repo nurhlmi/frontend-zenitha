5c78e5e3290890a9ad4a77513fa82376806b45dc
Hilmi <nurhilmi.mail@gmail.com>
2022-03-07 22:32:36 +0700

    add api banner & product

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -6,11 +7,62 @@ import "../style/Slider.css";
 
 import { ProductCard } from "../components/Card";
 
-import { Box, Container, Typography, Button, Grid, Card, CardActionArea, CardContent } from "@mui/material";
+import { Box, Container, Link, Typography, Button, Grid, Card, CardActionArea, CardContent } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import { apiUrl } from "../variable/Url";
 
 export default function Home(props) {
-   const settingSlider = {
+   const [banner, setBanner] = React.useState([]);
+   const [product, setProduct] = React.useState();
+   const [secondBanner, setSecondBanner] = React.useState([]);
+   const [footerBanner, setFooterBanner] = React.useState();
+
+   const getBanner = async () => {
+      await axios
+         .get(`${apiUrl}/banner/fetch`)
+         .then((res) => {
+            // console.log(res.data.data);
+            setBanner(res.data.data);
+         })
+         .catch((err) => {
+            console.log(err.response);
+         });
+   };
+   const getProduct = async () => {
+      await axios
+         .get(`${apiUrl}/product/fetch`)
+         .then((res) => {
+            // console.log(res.data.data);
+            setProduct(res.data.data);
+         })
+         .catch((err) => {
+            console.log(err.response);
+         });
+   };
+   const getSecondBanner = async () => {
+      await axios
+         .get(`${apiUrl}/setting/second_banner/fetch`)
+         .then((res) => {
+            // console.log(res.data.data);
+            setSecondBanner(res.data.data);
+         })
+         .catch((err) => {
+            console.log(err.response);
+         });
+   };
+   const getFooterBanner = async () => {
+      await axios
+         .get(`${apiUrl}/setting/footer_banner/fetch`)
+         .then((res) => {
+            // console.log(res.data.data);
+            setFooterBanner(res.data.data);
+         })
+         .catch((err) => {
+            console.log(err.response);
+         });
+   };
+
+   const settingBanner = {
       dots: true,
       autoplay: true,
       autoplaySpeed: 5000,
@@ -27,183 +79,74 @@ export default function Home(props) {
          },
       ],
    };
-   const settingCategory = {
-      arrows: true,
-      infinite: true,
-      slidesToShow: 8,
-      slidesToScroll: 1,
-      responsive: [
-         {
-            breakpoint: 1200,
-            settings: {
-               slidesToShow: 6,
-            },
-         },
-         {
-            breakpoint: 900,
-            settings: {
-               slidesToShow: 5,
-            },
-         },
-         {
-            breakpoint: 600,
-            settings: {
-               slidesToShow: 3,
-            },
-         },
-         {
-            breakpoint: 500,
-            settings: {
-               slidesToShow: 2,
-            },
-         },
-      ],
-   };
-   const settingPromo = {
-      arrows: true,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-         {
-            breakpoint: 900,
-            settings: {
-               slidesToShow: 2,
-            },
-         },
-      ],
-   };
-   const [products] = React.useState([
-      {
-         name: "Ummi 11",
-         slug: "ummi-11",
-         image: "image 8.png",
-         price: "350.000",
-         discount: "30%",
-         discount_price: "219.000",
-      },
-      {
-         name: "Long Blouse Adella 04",
-         slug: "long-blouse-adella-04",
-         image: "image 12.png",
-         price: "159.000",
-         discount: "20%",
-         discount_price: "127.000",
-      },
-      {
-         name: "Blouse Lavina 08",
-         slug: "blouse-lavina-08",
-         image: "image 13.png",
-         price: "149.000",
-         discount: "20%",
-         discount_price: "119.000",
-      },
-      {
-         name: "Gamis Kids Anissa 11",
-         slug: "gamis-kids-anissa-11",
-         image: "image 10.png",
-         price: "269.000",
-         discount: null,
-         discount_price: null,
-      },
-      {
-         name: "Znk 245",
-         slug: "znk-245",
-         image: "image 14.png",
-         price: "117.000",
-         discount: "20%",
-         discount_price: "82.000",
-      },
-   ]);
-   const [category] = React.useState([
-      {
-         name: "Atasan",
-         slug: "atasan",
-         image: "atasan.png",
-      },
-      {
-         name: "Bayi Ibu",
-         slug: "bayiibu",
-         image: "bayiibu.png",
-      },
-      {
-         name: "Gamis",
-         slug: "gamis",
-         image: "gamis.png",
-      },
-      {
-         name: "Hijab",
-         slug: "hijab",
-         image: "hijab.png",
-      },
-      {
-         name: "Jaket",
-         slug: "jaket",
-         image: "jaket.png",
-      },
-      {
-         name: "Koko",
-         slug: "koko",
-         image: "koko.png",
-      },
-      {
-         name: "Kulot",
-         slug: "kulot",
-         image: "kulot.png",
-      },
-      {
-         name: "Kurta",
-         slug: "kurta",
-         image: "kurta.png",
-      },
-      {
-         name: "Manset",
-         slug: "manset",
-         image: "manset.png",
-      },
-      {
-         name: "Masker",
-         slug: "masker",
-         image: "masker.png",
-      },
-      {
-         name: "Mukena",
-         slug: "mukena",
-         image: "mukena.png",
-      },
-      {
-         name: "Rok",
-         slug: "rok",
-         image: "rok.png",
-      },
-      {
-         name: "Rompi Sholat",
-         slug: "rompisholat",
-         image: "rompisholat.png",
-      },
-      {
-         name: "Sarimbit",
-         slug: "sarimbit",
-         image: "sarimbit.png",
-      },
-   ]);
+   // const settingCategory = {
+   //    arrows: true,
+   //    infinite: true,
+   //    slidesToShow: 8,
+   //    slidesToScroll: 1,
+   //    responsive: [
+   //       {
+   //          breakpoint: 1200,
+   //          settings: {
+   //             slidesToShow: 6,
+   //          },
+   //       },
+   //       {
+   //          breakpoint: 900,
+   //          settings: {
+   //             slidesToShow: 5,
+   //          },
+   //       },
+   //       {
+   //          breakpoint: 600,
+   //          settings: {
+   //             slidesToShow: 3,
+   //          },
+   //       },
+   //       {
+   //          breakpoint: 500,
+   //          settings: {
+   //             slidesToShow: 2,
+   //          },
+   //       },
+   //    ],
+   // };
+   // const settingPromo = {
+   //    arrows: true,
+   //    infinite: true,
+   //    slidesToShow: 3,
+   //    slidesToScroll: 1,
+   //    responsive: [
+   //       {
+   //          breakpoint: 900,
+   //          settings: {
+   //             slidesToShow: 2,
+   //          },
+   //       },
+   //    ],
+   // };
+
    React.useEffect(() => {
+      getBanner();
+      getProduct();
+      getSecondBanner();
+      getFooterBanner();
       window.scrollTo(0, 0);
    }, []);
+
    return (
       <React.Fragment>
          <Box sx={{ mb: 4 }}>
-            <Slider {...settingSlider}>
-               <Box component={RouterLink} to={`/article/read-promo`}>
-                  <img alt="slide" src="assets/images/banners/banner.jpg" />
-               </Box>
-               <Box component={RouterLink} to={`/article/read-promo`}>
-                  <img alt="slide" src="assets/images/banners/banner.jpg" />
-               </Box>
+            <Slider {...settingBanner}>
+               {banner.map((value, index) => (
+                  <Box component={Link} href={value.url} key={index}>
+                     <img alt={`Banner ${value.order}`} src={value.banner_url} />
+                  </Box>
+               ))}
             </Slider>
          </Box>
          <Container>
-            <Box sx={{ mb: 4 }}>
+            {/* <Box sx={{ mb: 4 }}>
                <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
                   Kategori Produk
                </Typography>
@@ -229,8 +172,8 @@ export default function Home(props) {
                      </div>
                   ))}
                </Slider>
-            </Box>
-            <Box sx={{ mb: 4 }}>
+            </Box> */}
+            {/* <Box sx={{ mb: 4 }}>
                <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
                   Lagi Promo
                </Typography>
@@ -263,61 +206,70 @@ export default function Home(props) {
                      </Box>
                   </Grid>
                </Grid>
-            </Box>
+            </Box> */}
             <Box sx={{ mb: 6 }}>
                <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
                   Produk Terbaru
                </Typography>
-               <Grid container spacing={{ xs: 1, sm: 2 }}>
-                  {products.map((value, index) => (
-                     <Grid item xs={6} sm={4} md={3} lg={2.4} key={index}>
-                        <ProductCard
-                           key={index}
-                           name={value.name}
-                           slug={value.slug}
-                           image={value.image}
-                           price={value.price}
-                           discount={value.discount}
-                           discount_price={value.discount_price}
-                        />
+               {product?.data.length > 0 ? (
+                  <>
+                     <Grid container spacing={{ xs: 1, sm: 2 }}>
+                        {product.data.map((value, index) => (
+                           <Grid item xs={6} sm={4} md={3} lg={2.4} key={index}>
+                              <ProductCard
+                                 key={index}
+                                 name={value.product_name}
+                                 slug={value.main_product.product_slug}
+                                 image={value.image}
+                                 price={value.price}
+                                 discount={null}
+                                 discount_price={null}
+                              />
+                           </Grid>
+                        ))}
                      </Grid>
-                  ))}
-               </Grid>
-               <Box sx={{ textAlign: "center", mt: 2 }}>
-                  <Button variant="outlined" component={RouterLink} to="/products">
-                     Lihat Semua
-                  </Button>
-               </Box>
+                     {product.meta.total > 10 && (
+                        <Box sx={{ textAlign: "center", mt: 2 }}>
+                           <Button variant="outlined" component={RouterLink} to="/products">
+                              Lihat Semua
+                           </Button>
+                        </Box>
+                     )}
+                  </>
+               ) : (
+                  <Typography textAlign="center" pt={2}>
+                     Belum ada produk terbaru
+                  </Typography>
+               )}
             </Box>
          </Container>
          <Grid container sx={{ mb: 6 }}>
-            <Grid item xs={12} md={6}>
-               <Box sx={{ mr: { xs: 0, md: 1 } }}>
-                  <img alt="Second Banner" src={`/assets/images/banners/second-banner1.jpg`} width="100%" />
-               </Box>
-            </Grid>
-            <Grid item xs={12} md={6}>
-               <Box sx={{ ml: { xs: 0, md: 1 } }}>
-                  <img alt="Second Banner" src={`/assets/images/banners/second-banner2.jpg`} width="100%" />
-               </Box>
-            </Grid>
+            {secondBanner.map((value, index) => (
+               <Grid item xs={12} md={6} key={index}>
+                  <Box sx={{ mr: { xs: 0, md: 1 } }}>
+                     <img alt={`Banner${value.order}`} src={value.banner} width="100%" />
+                  </Box>
+               </Grid>
+            ))}
          </Grid>
-         <Container>
-            <Grid container spacing={2} alignItems="center">
-               <Grid item xs={12} md={6}>
-                  <Typography fontWeight="bold" mb={2} sx={{ fontSize: { xs: "25px", sm: "30px" }, textTransform: "uppercase" }}>
-                     Brand yang tersedia di toko kami
-                  </Typography>
-                  <Typography color="text.secondary">
-                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex sunt maxime rem, officiis, voluptatibus itaque nostrum quaerat velit, vitae
-                     asperiores labore temporibus assumenda cumque debitis alias iste a similique quia.
-                  </Typography>
+         {footerBanner !== undefined && (
+            <Container>
+               <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={12} md={6}>
+                     <Typography fontWeight="bold" mb={2} sx={{ fontSize: { xs: "25px", sm: "30px" }, textTransform: "uppercase" }}>
+                        Brand yang tersedia di toko kami
+                     </Typography>
+                     {/* <Typography color="text.secondary">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex sunt maxime rem, officiis, voluptatibus itaque nostrum quaerat velit, vitae
+                        asperiores labore temporibus assumenda cumque debitis alias iste a similique quia.
+                     </Typography> */}
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                     <img alt="Footer Banner" src={footerBanner.banner} width="100%" />
+                  </Grid>
                </Grid>
-               <Grid item xs={12} md={6}>
-                  <img alt="Second Banner" src={`/assets/images/banners/bottom-banner.png`} width="100%" />
-               </Grid>
-            </Grid>
-         </Container>
+            </Container>
+         )}
       </React.Fragment>
    );
 }
