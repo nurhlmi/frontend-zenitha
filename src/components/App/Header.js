07caa7dd0f98a7def1dd5bb69560a683f4ff5b54
Hilmi <nurhilmi.mail@gmail.com>
2022-03-07 22:34:13 +0700

    add api category

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import axios from "axios";
 import {
    AppBar,
    Avatar,
@@ -19,7 +20,6 @@ import {
    InputBase,
    ListItemIcon,
 } from "@mui/material";
-// import { Logout, Notifications } from "@mui/icons-material";
 import {
    Logout,
    Search,
@@ -32,14 +32,12 @@ import {
    ShoppingBagOutlined,
 } from "@mui/icons-material";
 
-// import axios from "axios";
-// import { apiUrl } from "../../variable/Url";
+import { apiUrl } from "../../variable/Url";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 // import { useRecoilState } from "recoil";
 // import { authentication } from "../../store/Authentication";
 
 export default function Header(props) {
-   const navigate = useNavigate();
    const [anchorEl, setAnchorEl] = React.useState(null);
    const open = Boolean(anchorEl);
    const handleClick = (event) => {
@@ -49,57 +47,38 @@ export default function Header(props) {
       setAnchorEl(null);
    };
 
-   // const navigate = useNavigate();
+   const navigate = useNavigate();
    // eslint-disable-next-line no-unused-vars
    // const [auth, setAuth] = useRecoilState(authentication);
    // const token = localStorage.getItem("token");
    // const name = localStorage.getItem("name");
 
    const avatar = localStorage.getItem("avatar");
-   const [login, setLogin] = React.useState(true);
+   const [login, setLogin] = React.useState(false);
    const [modal, setModal] = React.useState(false);
    const [cart] = React.useState(1);
-   const [category] = React.useState({
-      oval: ["Gamis", "Gamis & Jilbab", "Jilbab", "Koko", "Aksesoris"],
-      kayana: ["Tunik", "Blouse", "Gamis", "Rok", "Hijab", "Aksesoris", "Masker", "Outer", "Outer & Rok", "Blouse & Rok"],
-      zenitha: [
-         "Gamis",
-         "Blouse",
-         "Jilbab",
-         "Dewasa",
-         "Koko",
-         "Aksesoris",
-         "Batita",
-         "Outer",
-         "Baby",
-         "Kids",
-         "Manset",
-         "Mukena",
-         "Rok",
-         "Gamis & Jilbab",
-         "Blouse & Celana",
-         "Tunik",
-         "Belia",
-         "Kemko",
-         "Zenitha Kids",
-         "Overall",
-      ],
-      adnin: ["Rompi Sholat", "Kurta", "Gamis"],
-      permata: ["Dewasa", "Sekolah", "Baby", "Remaja", "Berri Dewasa", "Remaja/Sekolah", "Gamis", "Jilbab", "Jilbab Sekolah", "Kids", "Berri Kids", "Batita"],
-   });
-   const CategoryItems = (props) => {
-      const brand = props.brand;
-      const category = props.category;
-      return (
-         <Grid item xs={6}>
-            <Typography variant="body2" color="text.secondary">
-               <Link component={RouterLink} to={`/category/${brand}/${category.toLowerCase()}`} underline="none" color="inherit">
-                  {category}
-               </Link>
-            </Typography>
-         </Grid>
-      );
+
+   const [category, setCategory] = React.useState([]);
+   const getCategory = async () => {
+      await axios
+         .get(`${apiUrl}/category/fetch`, {
+            params: {
+               with_sub: 1,
+            },
+         })
+         .then((res) => {
+            // console.log(res.data.data);
+            setCategory(res.data.data);
+         })
+         .catch((err) => {
+            console.log(err.response);
+         });
    };
+
+   React.useEffect(() => {
+      getCategory();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
 
    const handleChange = (e) => {
       setSearch(e.target.value);
@@ -147,6 +126,7 @@ export default function Header(props) {
                               </Box>
                            </Box>
                            <Button
+                              onClick={() => setModal(!modal)}
                               onMouseOver={() => setModal(true)}
                               color="inherit"
                               sx={{ display: { xs: "none", sm: "flex" }, alignItems: "center", p: 0.5, mx: 1 }}
@@ -161,98 +141,45 @@ export default function Header(props) {
                                  sx={{ width: "100%", bgcolor: "background.paper", mt: 10, p: 4, maxHeight: "70vh", overflow: "auto" }}
                                  onMouseLeave={() => setModal(false)}
                               >
-                                 <Grid container spacing={2}>
-                                    <Grid item xs={12} sm={6} md={4} lg mb={2}>
-                                       <Stack
-                                          direction="row"
-                                          divider={<Divider orientation="vertical" flexItem />}
-                                          spacing={1}
-                                          alignItems="center"
-                                          sx={{ pb: 1 }}
-                                       >
-                                          <img alt="OVAL" src="/assets/images/brands/oval.png" height="30" />
-                                          <Typography variant="button">OVAL</Typography>
-                                       </Stack>
-                                       <Divider />
-                                       <Grid container spacing={1} pt={1}>
-                                          {category.oval.map((value, index) => (
-                                             <CategoryItems brand="oval" category={value} key={index} />
-                                          ))}
-                                       </Grid>
+                                 {category.length > 0 ? (
+                                    <Grid container spacing={2}>
+                                       {category.map((value, index) => (
+                                          <Grid item xs={12} sm={6} md={4} lg mb={2} key={index}>
+                                             <Stack
+                                                direction="row"
+                                                divider={<Divider orientation="vertical" flexItem />}
+                                                spacing={1}
+                                                alignItems="center"
+                                                sx={{ pb: 1 }}
+                                             >
+                                                <img alt={value.category_name} src={value.image_url} height="30" />
+                                                <Typography variant="button">{value.category_name}</Typography>
+                                             </Stack>
+                                             <Divider />
+                                             <Grid container spacing={1} pt={1}>
+                                                {value.sub_category.map((row, index) => (
+                                                   <Grid item xs={6} key={index}>
+                                                      <Typography variant="body2" color="text.secondary">
+                                                         <Link
+                                                            component={RouterLink}
+                                                            to={`/category/${value.category_slug}/${row.sub_category_slug}`}
+                                                            underline="none"
+                                                            color="inherit"
+                                                         >
+                                                            {row.sub_category_name}
+                                                         </Link>
+                                                      </Typography>
+                                                   </Grid>
+                                                ))}
+                                             </Grid>
+                                          </Grid>
+                                       ))}
                                     </Grid>
-                                    <Grid item xs={12} sm={6} md={4} lg mb={2}>
-                                       <Stack
-                                          direction="row"
-                                          divider={<Divider orientation="vertical" flexItem />}
-                                          spacing={1}
-                                          alignItems="center"
-                                          sx={{ pb: 1 }}
-                                       >
-                                          <img alt="KAYANA" src="/assets/images/brands/kayana.png" height="30" />
-                                          <Typography variant="button">KAYANA</Typography>
-                                       </Stack>
-                                       <Divider />
-                                       <Grid container spacing={1} pt={1}>
-                                          {category.kayana.map((value, index) => (
-                                             <CategoryItems brand="kayana" category={value} key={index} />
-                                          ))}
-                                       </Grid>
-                                    </Grid>
-                                    <Grid item xs={12} sm={6} md={4} lg mb={2}>
-                                       <Stack
-                                          direction="row"
-                                          divider={<Divider orientation="vertical" flexItem />}
-                                          spacing={1}
-                                          alignItems="center"
-                                          sx={{ pb: 1 }}
-                                       >
-                                          <img alt="Zenitha" src="/assets/images/brands/zenitha.png" height="30" />
-                                          <Typography variant="button">Zenitha</Typography>
-                                       </Stack>
-                                       <Divider />
-                                       <Grid container spacing={1} pt={1}>
-                                          {category.zenitha.map((value, index) => (
-                                             <CategoryItems brand="zenitha" category={value} key={index} />
-                                          ))}
-                                       </Grid>
-                                    </Grid>
-                                    <Grid item xs={12} sm={6} md={4} lg mb={2}>
-                                       <Stack
-                                          direction="row"
-                                          divider={<Divider orientation="vertical" flexItem />}
-                                          spacing={1}
-                                          alignItems="center"
-                                          sx={{ pb: 1 }}
-                                       >
-                                          <img alt="ADNIN" src="/assets/images/brands/adnin.png" height="30" />
-                                          <Typography variant="button">ADNIN</Typography>
-                                       </Stack>
-                                       <Divider />
-                                       <Grid container spacing={1} pt={1}>
-                                          {category.adnin.map((value, index) => (
-                                             <CategoryItems brand="adnin" category={value} key={index} />
-                                          ))}
-                                       </Grid>
-                                    </Grid>
-                                    <Grid item xs={12} sm={6} md={4} lg mb={2}>
-                                       <Stack
-                                          direction="row"
-                                          divider={<Divider orientation="vertical" flexItem />}
-                                          spacing={1}
-                                          alignItems="center"
-                                          sx={{ pb: 1 }}
-                                       >
-                                          <img alt="PERMATA" src="/assets/images/brands/permata.png" height="30" />
-                                          <Typography variant="button">PERMATA</Typography>
-                                       </Stack>
-                                       <Divider />
-                                       <Grid container spacing={1} pt={1}>
-                                          {category.permata.map((value, index) => (
-                                             <CategoryItems brand="permata" category={value} key={index} />
-                                          ))}
-                                       </Grid>
-                                    </Grid>
-                                 </Grid>
+                                 ) : (
+                                    <Typography textAlign="center" mt={2}>
+                                       Belum ada kategori
+                                    </Typography>
+                                 )}
                               </Box>
                            </Modal>
                         </Box>
