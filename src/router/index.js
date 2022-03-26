03b86b0446ec9eeb9d2b3a0d61ddc9db631e6b0e
Hilmi <nurhilmi.mail@gmail.com>
2022-03-26 16:36:15 +0700

    add middleware, css, article & tutorial,& chekout

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import * as Middleware from "../middleware/App";
+import * as Middleware from "../middleware";
 import App from "../components/App";
 import Auth from "../components/Auth";
+import "../App.css";
 
 import Login from "../views/Auth/Login";
 import Register from "../views/Auth/Register";
@@ -20,16 +21,18 @@ import Settings from "../views/Settings";
 import SettingAddress from "../views/Settings/Address";
 import SettingPassword from "../views/Settings/Password";
 
-// import Article from "../views/Article";
-// import ArticleDetail from "../views/Article/Article";
-// import TutorialDetail from "../views/Article/Tutorial";
+import ArticleTutorial from "../views/Article/ArticleTutorial";
+import Article from "../views/Article/Article";
+import ArticleDetail from "../views/Article/ArticleDetail";
+import Tutorial from "../views/Article/Tutorial";
+import TutorialDetail from "../views/Article/TutorialDetail";
 
 import Wishlist from "../views/Wishlist";
 import Cart from "../views/Cart";
 import Order from "../views/Order";
 // import OrderDetail from "../views/Order/Detail";
 
-// import Checkout from "../views/Checkout";
+import Checkout from "../views/Checkout";
 // import Payment from "../views/Payment";
 
 function Router() {
@@ -37,9 +40,30 @@ function Router() {
       <BrowserRouter>
          <Routes>
             <Route path="/" exact element={<App render={<Home />} title="Busana Keluarga Muslim Trendy" />} />
-            <Route path="/login" element={<App render={<Auth render={<Login />} />} title="Masuk" />} />
-            <Route path="/register" element={<App render={<Auth render={<Register />} />} title="Daftar" />} />
-            <Route path="/reset-password" element={<App render={<Auth render={<ResetPassword />} />} title="Atur ulang kata sandi" />} />
+            <Route
+               path="/login"
+               element={
+                  <Middleware.Before>
+                     <App render={<Auth render={<Login />} />} title="Masuk" />
+                  </Middleware.Before>
+               }
+            />
+            <Route
+               path="/register"
+               element={
+                  <Middleware.Before>
+                     <App render={<Auth render={<Register />} />} title="Daftar" />
+                  </Middleware.Before>
+               }
+            />
+            <Route
+               path="/reset-password"
+               element={
+                  <Middleware.Before>
+                     <App render={<Auth render={<ResetPassword />} />} title="Atur ulang kata sandi" />
+                  </Middleware.Before>
+               }
+            />
 
             <Route path="/category" element={<App render={<Category />} title="Kategori" />} />
             <Route path="/category/:brand_id/:category_id" element={<App render={<CategoryDetail />} title="Detail Kategori" />} />
@@ -48,13 +72,69 @@ function Router() {
             <Route path="/products" element={<App render={<Products />} title="Semua Produk" />} />
             <Route path="/product/:slug" element={<App render={<ProductDetail />} title="Detail Produk" />} />
 
-            <Route path="/settings" element={<App render={<Settings />} title="Pengaturan" />} />
-            <Route path="/settings/address" element={<App render={<SettingAddress />} title="Daftar Alamat" />} />
-            <Route path="/settings/password" element={<App render={<SettingPassword />} title="Ganti Password" />} />
+            <Route path="/article-tutorial" element={<App render={<ArticleTutorial />} title="Artikel & Tutorial" />} />
+            <Route path="/article" element={<App render={<Article />} title="Artikel" />} />
+            <Route path="/article/:slug" element={<App render={<ArticleDetail />} title="Detail Artikel" />} />
+            <Route path="/tutorial" element={<App render={<Tutorial />} title="Tutorial" />} />
+            <Route path="/tutorial/:slug" element={<App render={<TutorialDetail />} title="Detail Tutorial" />} />
 
-            <Route path="/wishlist" element={<App render={<Wishlist />} title="Wishlist" />} />
-            <Route path="/cart" element={<App render={<Cart />} title="Keranjang" />} />
-            <Route path="/order" element={<App render={<Order />} title="Pesanan" />} />
+            <Route
+               path="/settings"
+               element={
+                  <Middleware.After>
+                     <App render={<Settings />} title="Pengaturan" />
+                  </Middleware.After>
+               }
+            />
+            <Route
+               path="/settings/address"
+               element={
+                  <Middleware.After>
+                     <App render={<SettingAddress />} title="Daftar Alamat" />
+                  </Middleware.After>
+               }
+            />
+            <Route
+               path="/settings/password"
+               element={
+                  <Middleware.After>
+                     <App render={<SettingPassword />} title="Ganti Password" />
+                  </Middleware.After>
+               }
+            />
+
+            <Route
+               path="/wishlist"
+               element={
+                  <Middleware.After>
+                     <App render={<Wishlist />} title="Wishlist" />
+                  </Middleware.After>
+               }
+            />
+            <Route
+               path="/cart"
+               element={
+                  <Middleware.After>
+                     <App render={<Cart />} title="Keranjang" />
+                  </Middleware.After>
+               }
+            />
+            <Route
+               path="/checkout"
+               element={
+                  <Middleware.After>
+                     <App render={<Checkout />} title="Checkout" />
+                  </Middleware.After>
+               }
+            />
+            <Route
+               path="/order"
+               element={
+                  <Middleware.After>
+                     <App render={<Order />} title="Pesanan" />
+                  </Middleware.After>
+               }
+            />
          </Routes>
       </BrowserRouter>
    );
