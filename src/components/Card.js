71a156538ab735089334b157628b1c42d75ba171
Hilmi <nurhilmi.mail@gmail.com>
2022-03-31 00:59:47 +0700

    fix discount & remove WishlistCard

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Box, Typography, Button, IconButton, Tooltip, Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
-import { DeleteOutlineRounded } from "@mui/icons-material";
+import { Box, Typography, Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import { NumberFormat } from "./Format";
 
 function ProductCard(props) {
+   let discount = null;
+   let percent = null;
+   if (props.discount !== null) {
+      if (props.discount_type === "rp") {
+         discount = props.price - props.discount;
+         percent = Math.floor((props.discount / props.price) * 100);
+      } else {
+         discount = props.price - (props.price * props.discount) / 100;
+         percent = props.discount;
+      }
+   }
    return (
       <Card sx={{ height: "100%" }}>
          <CardActionArea component={RouterLink} to={`/product/${props.slug}`} sx={{ height: "100%" }}>
@@ -14,13 +24,13 @@ function ProductCard(props) {
                   {props.name}
                </Typography>
                <Typography gutterBottom variant="subtitle2" component="div" fontWeight="bold">
-                  {props.discount !== null ? NumberFormat(props.discount_price) : NumberFormat(props.price)}
+                  {props.discount !== null ? NumberFormat(discount) : NumberFormat(props.price)}
                </Typography>
                {props.discount !== null && (
                   <React.Fragment>
                      <Box sx={{ display: "inline", background: "#ffeaef", borderRadius: 0.5, px: 0.5, pb: 0.4, mr: 1 }}>
                         <Typography variant="caption" color="#ff5c84" fontWeight="bold">
-                           {props.discount}
+                           {percent}%
                         </Typography>
                      </Box>
                      <Typography variant="caption" color="text.secondary">
@@ -34,48 +44,4 @@ function ProductCard(props) {
    );
 }
 
-function WishlistCard(props) {
-   return (
-      <Card sx={{ height: "100%" }}>
-         <CardActionArea component={RouterLink} to={`/product/${props.slug}`} sx={{ height: "100%" }}>
-            <CardMedia component="img" height={{ xs: 150, sm: 200 }} image={`/assets/images/products/${props.image}`} alt={props.name} />
-            <CardContent>
-               <Box sx={{ diplay: "flex", alignContent: "space-between" }}>
-                  <Box>
-                     <Typography gutterBottom variant="body2" wrap="nowrap">
-                        {props.name}
-                     </Typography>
-                     <Typography gutterBottom variant="subtitle2" component="div" fontWeight="bold">
-                        {props.discount !== null ? `Rp${props.discount_price}` : `Rp${props.price}`}
-                     </Typography>
-                     {props.discount !== null && (
-                        <React.Fragment>
-                           <Box sx={{ display: "inline", background: "#ffeaef", borderRadius: 0.5, p: 0.5, mr: 1 }}>
-                              <Typography variant="caption" color="#ff5c84" fontWeight="bold">
-                                 {props.discount}
-                              </Typography>
-                           </Box>
-                           <Typography variant="caption" color="text.secondary">
-                              <del>Rp{props.price}</del>
-                           </Typography>
-                        </React.Fragment>
-                     )}
-                  </Box>
-               </Box>
-               <Box sx={{ display: "flex", alignItems: "flex-end", alignContent: "flex-end", mt: 2 }}>
-                  <Tooltip title="Hapus">
-                     <IconButton sx={{ mr: 0.5 }} onClick={(e) => e.preventDefault()}>
-                        <DeleteOutlineRounded fontSize="small" />
-                     </IconButton>
-                  </Tooltip>
-                  <Button variant="outlined" fullWidth onClick={(e) => e.preventDefault()}>
-                     <Typography variant="body2">+ Keranjang</Typography>
-                  </Button>
-               </Box>
-            </CardContent>
-         </CardActionArea>
-      </Card>
-   );
-}
-
-export { ProductCard, WishlistCard };
+export { ProductCard };
