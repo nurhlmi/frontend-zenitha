5eee6d3ce4ec0eebd85b4fda06526e75e9aa36fa
Hilmi <nurhilmi.mail@gmail.com>
2022-03-31 03:52:52 +0700

    fix minor

--- src/views/Wishlist.js
+++ src/views/Wishlist.js
@@ -2,49 +2,49 @@ import React from "react";
 import { Container, Typography, Grid } from "@mui/material";
 // import { Link as RouterLink } from "react-router-dom";
 
-import { WishlistCard } from "../components/Card";
+import { ProductCard } from "../components/Card";
 
 export default function Wishlist(props) {
    const [products] = React.useState([
       {
          name: "Ummi 11",
          slug: "ummi-11",
-         image: "image 8.png",
-         price: "350.000",
-         discount: "30%",
-         discount_price: "219.000",
+         image: window.location.origin + "/assets/images/products/image 8.png",
+         price: 350000,
+         discount: 10,
+         discount_type: "percent",
       },
       {
          name: "Long Blouse Adella 04",
          slug: "long-blouse-adella-04",
-         image: "image 12.png",
-         price: "159.000",
-         discount: "20%",
-         discount_price: "127.000",
+         image: window.location.origin + "/assets/images/products/image 12.png",
+         price: 159000,
+         discount: 10,
+         discount_type: "percent",
       },
       {
          name: "Blouse Lavina 08",
          slug: "blouse-lavina-08",
-         image: "image 13.png",
-         price: "149.000",
-         discount: "20%",
-         discount_price: "119.000",
+         image: window.location.origin + "/assets/images/products/image 13.png",
+         price: 149000,
+         discount: 10,
+         discount_type: "percent",
       },
       {
          name: "Gamis Kids Anissa 11",
          slug: "gamis-kids-anissa-11",
-         image: "image 10.png",
-         price: "269.000",
-         discount: null,
-         discount_price: null,
+         image: window.location.origin + "/assets/images/products/image 10.png",
+         price: 269000,
+         discount: 10,
+         discount_type: "percent",
       },
       {
          name: "Znk 245",
          slug: "znk-245",
-         image: "image 14.png",
-         price: "117.000",
-         discount: "20%",
-         discount_price: "82.000",
+         image: window.location.origin + "/assets/images/products/image 14.png",
+         price: 117000,
+         discount: 10,
+         discount_type: "percent",
       },
    ]);
    React.useEffect(() => {
@@ -58,14 +58,14 @@ export default function Wishlist(props) {
          <Grid container spacing={{ xs: 1, sm: 2 }}>
             {products.map((value, index) => (
                <Grid item xs={6} sm={4} md={3} lg={2.4} key={index}>
-                  <WishlistCard
+                  <ProductCard
                      key={index}
                      name={value.name}
                      slug={value.slug}
                      image={value.image}
                      price={value.price}
                      discount={value.discount}
-                     discount_price={value.discount_price}
+                     discount_type={value.discount_type}
                   />
                </Grid>
             ))}
