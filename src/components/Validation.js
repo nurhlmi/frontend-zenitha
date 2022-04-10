cd098a51e24147bfaf8a65f16abcde5fe459ecb5
Hilmi <nurhilmi.mail@gmail.com>
2022-04-10 23:57:38 +0700

    update label & add validation

--- src/components/Validation.js
+++ src/components/Validation.js
@@ -8,9 +8,9 @@ function Validation(error) {
       } else if (error[0] === "The email has already been taken.") {
          output = "Email telah digunakan.";
       } else if (error[0] === "The phone number field is required.") {
-         output = "Masukkan nomor telepon.";
+         output = "Masukkan nomor hp.";
       } else if (error[0] === "The phone number must be an integer.") {
-         output = "Masukkan nomor telepon.";
+         output = "Masukkan nomor hp.";
       } else if (error[0] === "The old password field is required.") {
          output = "Masukkan kata sandi saat ini.";
       } else if (error[0] === "old password is invalid") {
@@ -25,6 +25,20 @@ function Validation(error) {
          output = "Konfirmasi kata sandi dengan benar.";
       } else if (error[0] === "The password confirmation must be at least 8 characters.") {
          output = "Masukkan kata sandi minimal 8 karakter.";
+      } else if (error[0] === "The label field is required.") {
+         output = "Masukkan label alamat.";
+      } else if (error[0] === "The recipients name field is required.") {
+         output = "Masukkan nama penerima.";
+      } else if (error[0] === "The province id field is required.") {
+         output = "Pilih provinsi.";
+      } else if (error[0] === "The city id field is required.") {
+         output = "Pilih kabupaten/kota.";
+      } else if (error[0] === "The district id field is required.") {
+         output = "Pilih kecamatan.";
+      } else if (error[0] === "The postal code field is required.") {
+         output = "Masukkan kode pos.";
+      } else if (error[0] === "The address field is required.") {
+         output = "Masukkan alamat.";
       }
    }
    return output;
