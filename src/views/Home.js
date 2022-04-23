3e8427d0c3f93c978fef288fa07ef5685e7a3fbb
Hilmi <nurhilmi.mail@gmail.com>
2022-04-24 01:19:20 +0700

    add product highlight

--- src/views/Home.js
+++ src/views/Home.js
@@ -13,8 +13,10 @@ import { apiUrl } from "../variable/Url";
 import { LocalMallOutlined } from "@mui/icons-material";
 
 export default function Home(props) {
+   const token = localStorage.getItem("token");
    const [banner, setBanner] = React.useState([]);
    const [product, setProduct] = React.useState();
+   const [productHighlight, setProductHighlight] = React.useState();
    const [secondBanner, setSecondBanner] = React.useState([]);
    const [footerBanner, setFooterBanner] = React.useState();
 
@@ -24,6 +26,21 @@ export default function Home(props) {
          .then((res) => {
             // console.log(res.data.data);
             setBanner(res.data.data);
+         })
+         .catch((err) => {
+            console.log(err.response);
+         });
+   };
+   const getProductHighlight = async () => {
+      await axios
+         .get(`${apiUrl}/product_slider/fetch`, {
+            headers: {
+               Authorization: "Bearer " + token,
+            },
+         })
+         .then((res) => {
+            // console.log(res.data);
+            setProductHighlight(res.data);
          })
          .catch((err) => {
             console.log(err.response);
@@ -80,59 +97,15 @@ export default function Home(props) {
          },
       ],
    };
-   // const settingCategory = {
-   //    arrows: true,
-   //    infinite: true,
-   //    slidesToShow: 8,
-   //    slidesToScroll: 1,
-   //    responsive: [
-   //       {
-   //          breakpoint: 1200,
-   //          settings: {
-   //             slidesToShow: 6,
-   //          },
-   //       },
-   //       {
-   //          breakpoint: 900,
-   //          settings: {
-   //             slidesToShow: 5,
-   //          },
-   //       },
-   //       {
-   //          breakpoint: 600,
-   //          settings: {
-   //             slidesToShow: 3,
-   //          },
-   //       },
-   //       {
-   //          breakpoint: 500,
-   //          settings: {
-   //             slidesToShow: 2,
-   //          },
-   //       },
-   //    ],
-   // };
-   // const settingPromo = {
-   //    arrows: true,
-   //    infinite: true,
-   //    slidesToShow: 3,
-   //    slidesToScroll: 1,
-   //    responsive: [
-   //       {
-   //          breakpoint: 900,
-   //          settings: {
-   //             slidesToShow: 2,
-   //          },
-   //       },
-   //    ],
-   // };
 
    React.useEffect(() => {
       getBanner();
+      getProductHighlight();
       getProduct();
       getSecondBanner();
       getFooterBanner();
       window.scrollTo(0, 0);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
 
    return (
@@ -147,80 +120,42 @@ export default function Home(props) {
             </Slider>
          </Box>
          <Container>
-            {/* <Box sx={{ mb: 4 }}>
-               <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
-                  Kategori Produk
-               </Typography>
-               <Slider {...settingCategory}>
-                  {category.map((value, index) => (
-                     <div key={index}>
-                        <Card key={index} sx={{ mx: 1, border: "1px solid #e0e0e0", boxShadow: "none" }}>
-                           <CardActionArea component={RouterLink} to={`/category/zenitha/${value.slug}`}>
-                              <CardContent>
-                                 <Box
-                                    sx={{
-                                       width: "100%",
-                                       height: "100px",
-                                       backgroundPosition: "center center",
-                                       backgroundRepeat: "no-repeat",
-                                       backgroundImage: `url(/assets/images/categories/${value.image})`,
-                                    }}
-                                 />
-                                 <Typography variant="subtitle2">{value.name}</Typography>
-                              </CardContent>
-                           </CardActionArea>
-                        </Card>
-                     </div>
-                  ))}
-               </Slider>
-            </Box> */}
-            {/* <Box sx={{ mb: 4 }}>
-               <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
-                  Lagi Promo
-               </Typography>
-               <Grid container alignItems="center">
-                  <Grid item xs={0} sm={4} md={3} sx={{ display: { xs: "none", sm: "block" } }}>
-                     <img alt="Logo" src={`/assets/images/logo-zeni-promo.jpg`} width="100%" />
+            {productHighlight !== undefined && productHighlight.data.length > 0 && (
+               <Box sx={{ mb: 4 }}>
+                  <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
+                     Lagi Promo
+                  </Typography>
+                  <Grid container spacing={{ xs: 1, sm: 2 }}>
+                     <Grid item xs={0} sm={4} md={3} sx={{ display: { xs: "none", sm: "block" } }}>
+                        <img alt="Logo" src={`/assets/images/logo-zeni-promo.jpg`} width="100%" />
+                     </Grid>
+                     {productHighlight.data.map((value, index) => (
+                        <Grid item xs={6} sm={4} md={3} key={index}>
+                           <ProductCard
+                              name={value.product.product_name}
+                              slug={value.product.main_product.product_slug}
+                              image={value.product.image}
+                              price={value.product.price}
+                              discount={value.product.discount}
+                              discount_type={value.product.discount_type}
+                           />
+                        </Grid>
+                     ))}
                   </Grid>
-                  <Grid item xs={12} sm={8} md={9}>
-                     <Box sx={{ display: "flex", alignItems: "center" }}>
-                        <Slider {...settingPromo}>
-                           {products.map(
-                              (value, index) =>
-                                 value.discount !== null && (
-                                    <div key={index}>
-                                       <Box sx={{ mx: { xs: 0.5, sm: 1 } }}>
-                                          <ProductCard
-                                             key={index}
-                                             name={value.name}
-                                             slug={value.slug}
-                                             image={value.image}
-                                             price={value.price}
-                                             discount={value.discount}
-                                             discount_price={value.discount_price}
-                                          />
-                                       </Box>
-                                    </div>
-                                 )
-                           )}
-                        </Slider>
-                     </Box>
-                  </Grid>
-               </Grid>
-            </Box> */}
+               </Box>
+            )}
             <Box sx={{ mb: 6 }}>
                <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
                   Produk Terbaru
                </Typography>
                {product !== undefined ? (
                   <React.Fragment>
-                     {product?.data.length > 0 ? (
+                     {product.data.length > 0 ? (
                         <React.Fragment>
                            <Grid container spacing={{ xs: 1, sm: 2 }}>
                               {product.data.map((value, index) => (
                                  <Grid item xs={6} sm={4} md={3} lg={2.4} key={index}>
                                     <ProductCard
-                                       key={index}
                                        name={value.product_name}
                                        slug={value.main_product.product_slug}
                                        image={value.image}
