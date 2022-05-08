df85977f8ad2213997c8985376f635510c269b8d
Hilmi <nurhilmi.mail@gmail.com>
2022-05-08 15:51:07 +0700

    fix false

--- src/store/Search.js
+++ src/store/Search.js
@@ -3,7 +3,7 @@ import { Param } from "../components/Param";
 
 const search = atom({
    key: "search",
-   default: Param("q") !== false && decodeURI(Param("q")),
+   default: Param("q") !== false ? decodeURI(Param("q")) : "",
 });
 
 export { search };
