2fbf164699bb52be1278c2b42b3749de74cd5762
root <root@vps.server.local>
2022-06-01 14:36:34 +0000

    delete Url

--- src/variable/Url.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = `http://localhost:8000`;
-const apiUrl = `${baseUrl}/api`;
-const title = ` | Zenitha`;
-const token = localStorage.getItem("token");
-
-export { baseUrl, apiUrl, title, token };
