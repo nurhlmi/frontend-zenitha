bfcc7dedeb08e95b3456c44d647cef35987ea833
Hilmi <nurhilmi.mail@gmail.com>
2022-07-02 17:19:38 +0700

    fix after middleware

--- src/middleware/After.js
+++ src/middleware/After.js
@@ -1,16 +1,12 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
+import { Navigate } from "react-router";
 import { useRecoilValue } from "recoil";
 import { authentication } from "../store/Authentication";
 
 function After(props) {
-   const navigate = useNavigate();
    const { auth } = useRecoilValue(authentication);
-   useEffect(() => {
-      if (!auth) {
-         navigate("/login");
-      }
-   }, [auth, navigate]);
+   if (!auth) {
+      return <Navigate to="/" />;
+   }
 
    return props.children;
 }
